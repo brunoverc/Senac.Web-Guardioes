f4fd150f368c92c1bbe4ebb0b418051bcdf5da0a
Bruno Verçosa <69854207+brunoverc@users.noreply.github.com>
2024-01-10 21:42:29 -0300

    Primeira parte

--- script/index.js
+++ script/index.js
@@ -6,3 +6,33 @@ const members = [
   { name: 'Rocket Raccoon', id: 'rocket' },
   { name: 'I am Groot', id: 'groot' },
 ]
+
+let activeMember = 0;
+const images = document.getElementById('images');
+const memberName = document.getElementById('member__name');
+
+function changeMember(memberId){
+  activeMember = memberId;
+  const member = members[activeMember];
+  console.log(activeMember, member);
+
+  images.style.transform = `translateY(${-100 * activeMember}vh)`;
+  //alterar a classe do member name
+  //alterar o nome escrito
+
+  changeStatusButtons();
+}
+
+function changeStatusButtons(){
+  const prev = document.getElementById('button__prev');
+  const isFirst = activeMember === 0;
+  prev.disabled = isFirst; 
+
+  const next = document.getElementById('button__next');
+  const isLast = activeMember === members.length - 1; 
+  next.disabled = isLast;
+}
+
+function navigationMember(direction){
+  changeMember(activeMember + direction);
+}
