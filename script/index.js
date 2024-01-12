9d16a043996a4205df99b244221094157f9d01d4
Bruno Verçosa <69854207+brunoverc@users.noreply.github.com>
2024-01-11 21:11:00 -0300

    Finalizado

--- script/index.js
+++ script/index.js
@@ -9,7 +9,20 @@ const members = [
 
 let activeMember = 0;
 const images = document.getElementById('images');
+const menu = document.getElementById('menu');
+const navigation = document.getElementById('navigation');
 const memberName = document.getElementById('member__name');
+
+function changeStatusButtons(){
+  const prev = document.getElementById('button__prev');
+  const isFirst = activeMember === 0;
+  prev.disabled = isFirst;
+
+  const next = document.getElementById('button__next');
+  const isLast = activeMember === members.length - 1; 
+  next.disabled = isLast;
+
+}
 
 function changeMember(memberId){
   activeMember = memberId;
@@ -17,22 +30,22 @@ function changeMember(memberId){
   console.log(activeMember, member);
 
   images.style.transform = `translateY(${-100 * activeMember}vh)`;
-  //alterar a classe do member name
-  //alterar o nome escrito
+  memberName.classList = member.id;
+  changeName(member.name);
 
   changeStatusButtons();
 }
 
-function changeStatusButtons(){
-  const prev = document.getElementById('button__prev');
-  const isFirst = activeMember === 0;
-  prev.disabled = isFirst; 
-
-  const next = document.getElementById('button__next');
-  const isLast = activeMember === members.length - 1; 
-  next.disabled = isLast;
+function navigationMember(direction){
+  changeMember(activeMember + direction)
 }
 
-function navigationMember(direction){
-  changeMember(activeMember + direction);
+function changeMenu(){
+  menu.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
+
+function setMember(memberId){
+  changeMember(memberId);
+  changeMenu();
 }
